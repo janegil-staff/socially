de7a32e20bf40276804cd5d3e369881787964f74
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-06-21 22:32:51 +0200

    Handle Current Friend

--- frontend/src/store/actions/messengerAction.js
+++ frontend/src/store/actions/messengerAction.js
@@ -4,7 +4,7 @@ import { FRIEND_GET_SUCCESS } from "../types/messengerType";
 export const getFriends = () => async (dispatch) => {
   try {
     const response = await axios.get(
-      `${process.env.REACT_APP_API_ENDPOINT}/messenger/get-friends`
+      `${process.env.REACT_APP_API_ENDPOINT}(messenger/get-friends`
     );
     dispatch({
       type: FRIEND_GET_SUCCESS,
